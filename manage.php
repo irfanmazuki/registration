3b322d111b74950013b9572d08ee8bfa8dd500c9
Afugegege <afugegege@gmail.com>
2023-12-09 12:11:53 +0800

    try use post

--- manage.php
+++ manage.php
@@ -15,26 +15,14 @@
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/js/all.min.js"></script>
-    <script>
-     function view(id){
-      $("#selectedViewId").attr("value", id);
-      <?php
-        	$con = new mysqli("localhost", "root", "", "test");
-	
-          if ($con->connect_error) {
-            echo "$conn->connect_error";
-            die("Connection Failed: " . $con->connect_error);
-          }
-          
-          $query = "SELECT * FROM student";
-          $result = $con->query($query);
-      ?>
-     }
-  </script>
 </head>
 <body> 
   <!-- HIDDEN INPUT -->
-  <input id="selectedViewId" type="text" value="" hidden/>
+  <iframe name="dummyframe" id="dummyframe" style="display: none;"></iframe>
+  <form id="varForm" action="manage.php" target="dummyframe" method="POST">
+    <input id="selectedViewId" type="text" value="" name="selectedViewId" hidden/>
+    <input id="varSubmit" type="submit" hidden/>
+  </form>
     <div class="container custom-container">
         <h3 class="mb-4 pb-2 pb-md-0 mb-md-5 form-title">Debug Lab Registration Form</h3>
         <div class="table-wrapper">
@@ -80,8 +68,16 @@
 		echo "0 result";
 	}
 	
+  function refreshdata(){
+    if($_SERVER["REQUEST_METHOD"] == "POST") {
+      echo '<script>'.$_POST['selectedViewId'].'</script>';
+    }
+    
+  }
+
 	$con->close();
 ?>
+
 
 </tbody>
   
@@ -149,4 +145,13 @@
               </div>
             </div> 
 </body>
+<script>
+     function view(id){
+      $("#selectedViewId").attr("value", id);
+      $("#varForm").submit();
+      <?php
+      refreshdata();
+      ?>
+     }
+  </script>
 </html>
