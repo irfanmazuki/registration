305f63cbd1e7d4e6c472a8226c6f1f8acff38966
Afugegege <afugegege@gmail.com>
2023-12-09 11:13:54 +0800

    add view

--- manage.php
+++ manage.php
@@ -15,8 +15,26 @@
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/js/all.min.js"></script>
+    <script>
+     function view(id){
+      $("#selectedViewId").attr("value", id);
+      <?php
+        	$con = new mysqli("localhost", "root", "", "test");
+	
+          if ($con->connect_error) {
+            echo "$conn->connect_error";
+            die("Connection Failed: " . $con->connect_error);
+          }
+          
+          $query = "SELECT * FROM student";
+          $result = $con->query($query);
+      ?>
+     }
+  </script>
 </head>
 <body> 
+  <!-- HIDDEN INPUT -->
+  <input id="selectedViewId" type="text" value="" hidden/>
     <div class="container custom-container">
         <h3 class="mb-4 pb-2 pb-md-0 mb-md-5 form-title">Debug Lab Registration Form</h3>
         <div class="table-wrapper">
@@ -52,9 +70,9 @@
       $date=date_create($row["Birthday"]);
 
 			echo "<tr><td>".$row["Id"]."</td><td>".$row["first_Name"]."</td><td>".$row["Last_Name"]."</td><td>".date_format($date,"d M Y")."</td><td>".$row["Gender"]."</td><td>".$row["Email"]."</td><td>".$row["Phone_number"]."</td><td>".$row["Course"]."</td><td>
-                  <i class='fa-solid fa-eye'></i>
-                  <i class='fa-solid fa-pen-to-square'></i>
-                  <i class='fa-solid fa-trash'></i>
+                  <i type='button' onClick='view(".$row["Id"].")' data-toggle='modal' data-target='#viewPop' class='fa-solid fa-eye'></i>
+                  <i type='button' onClick='edit(".$row["Id"].")' data-toggle='modal' data-target='#editPop' class='fa-solid fa-pen-to-square'></i>
+                  <i type='button' onClick='delete(".$row["Id"].")' data-toggle='modal' data-target='#deletePop' class='fa-solid fa-trash'></i>
                   </td></tr>";
 		}
 		echo "</table>";
@@ -66,11 +84,69 @@
 ?>
 
 </tbody>
-     
-            </table>
+  
+          </table>
         </div>
       </div>    
-    </div> 
-	
+    </div>
+
+    <div class="modal fade" id="viewPop" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+              <div class="modal-dialog modal-dialog-centered" role="document">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalCenterTitle">WARNING</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                  </div>
+                  <div class="modal-body">
+                    Are you sure you want view data ?
+                  </div>
+                  <div class="modal-footer">
+                    <button type="button" style="border-width: 0px;" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" style="background-color: #ff9d0d; border-width: 0px;" class="btn btn-primary">View</button>
+                  </div>
+                </div>
+              </div>
+            </div> 
+            <div class="modal fade" id="editPop" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+              <div class="modal-dialog modal-dialog-centered" role="document">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalCenterTitle">WARNING</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                  </div>
+                  <div class="modal-body">
+                    Are you sure you want edit data ?
+                  </div>
+                  <div class="modal-footer">
+                    <button type="button" style="border-width: 0px;" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" style="background-color: #ff9d0d; border-width: 0px;" class="btn btn-primary">Edit</button>
+                  </div>
+                </div>
+              </div>
+            </div> 
+    
+    <div class="modal fade" id="deletePop" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+              <div class="modal-dialog modal-dialog-centered" role="document">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalCenterTitle">WARNING</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                  </div>
+                  <div class="modal-body">
+                    Are you sure you want to delete ?
+                  </div>
+                  <div class="modal-footer">
+                    <button type="button" style="border-width: 0px;" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" style="background-color: #ff9d0d; border-width: 0px;" class="btn btn-primary">Delete</button>
+                  </div>
+                </div>
+              </div>
+            </div> 
 </body>
 </html>
